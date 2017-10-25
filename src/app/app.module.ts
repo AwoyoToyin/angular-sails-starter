47fb81884a89a5188c650446e230858b84979fd0
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2017-10-25 14:27:04 +0100

    code formatting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { BaseService } from './shared/services/base.service';
-import { AppErrorHandler } from './shared/app-error-handler';
-import { routing } from './app.router';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.router';
+import { AppErrorHandler } from './shared/app-error-handler';
+import { BaseService } from './shared/services/base.service';
 
 @NgModule({
   declarations: [
