2fb53c7558716ffbc8be682dd5239dfd87bd8581
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2017-11-02 17:49:21 +0100

    configured the animation package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { supportDocument } from 'ack-angular-fx/web-animations.min';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,5 +8,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+// animation polyfill by ack-angular-fx
+supportDocument(document);
 
 platformBrowserDynamic().bootstrapModule(AppModule);
